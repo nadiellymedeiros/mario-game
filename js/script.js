d89f3337d8e6f3d7e7944e8bb3da182f82cb3a6e
nadiellymedeiros <nadiellymedeiros@gmail.com>
2022-11-09 14:15:49 -0300

    start game

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+
+const jump = () => {
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", ""); /* + converte a string para números*/
+
+  if (pipePosition <= 124 && pipePosition > 0 && marioPosition < 90) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "./imagens/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
+
+    clearInterval(loop);
+  }
+}, 10);
+
+document.addEventListener("keydown", jump);
